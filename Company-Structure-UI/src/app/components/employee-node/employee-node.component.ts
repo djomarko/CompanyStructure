28a1a318b8526041f335f0aa37530e5af257754f
djomarko <djo.marko@gmail.com>
2017-09-16 19:46:36 +1000

    adding some bootstrap styles in

--- Company-Structure-UI/src/app/components/employee-node/employee-node.component.ts
+++ Company-Structure-UI/src/app/components/employee-node/employee-node.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 import { EmployeeData } from './../../common/EmployeeData';
 
 @Component({
 	selector: 'app-employee-node',
 	templateUrl: './employee-node.component.html',
-	styleUrls: ['./employee-node.component.css']
+	styleUrls: ['./employee-node.component.less'],
 })
 export class EmployeeNodeComponent {
 
