278206d18df9501424e7e8480d406338d17a4e1d
djomarko <djo.marko@gmail.com>
2017-09-16 13:06:39 +1000

    initiate project

---
+++ Company-Structure-REST/src/main/java/com/momenton/companystructure/CompanystructureApplication.java
@@ -0,0 +1,12 @@
+package com.momenton.companystructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanystructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanystructureApplication.class, args);
+	}
+}
---
+++ Company-Structure-REST/src/test/java/com/momenton/companystructure/CompanystructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.momenton.companystructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanystructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
